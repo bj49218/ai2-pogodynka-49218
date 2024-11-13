304cdc1fda1efbc1dcc56b6395165b67a727a485
Julia Bródka <bj49218>
2024-11-14 00:32:09 +0100

    lab-e

--- src/Controller/MeasurementController.php
+++ src/Controller/MeasurementController.php
@@ -16,6 +16,7 @@ final class MeasurementController extends AbstractController
 {
 
     #[Route(name: 'app_measurement_index', methods: ['GET'])]
+    #[IsGranted('ROLE_MEASUREMENT_INDEX')]
     public function index(MeasurementRepository $measurementRepository): Response
     {
         return $this->render('measurement/index.html.twig', [
@@ -24,10 +25,11 @@ final class MeasurementController extends AbstractController
     }
 
     #[Route('/new', name: 'app_measurement_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_MEASUREMENT_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $measurement = new Measurement();
-        $form = $this->createForm(MeasurementTcdype::class, $measurement,[
+        $form = $this->createForm(MeasurementType::class, $measurement,[
             'validation_groups' => 'create',
         ]);
         $form->handleRequest($request);
@@ -46,6 +48,7 @@ final class MeasurementController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_measurement_show', methods: ['GET'])]
+    #[IsGranted('ROLE_MEASUREMENT_SHOW')]
     public function show(Measurement $measurement): Response
     {
         return $this->render('measurement/show.html.twig', [
@@ -54,6 +57,7 @@ final class MeasurementController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_measurement_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_MEASUREMENT_EDIT')]
     public function edit(Request $request, Measurement $measurement, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MeasurementType::class, $measurement,[
@@ -74,6 +78,7 @@ final class MeasurementController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_measurement_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_MEASUREMENT_DELETE')]
     public function delete(Request $request, Measurement $measurement, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$measurement->getId(), $request->getPayload()->getString('_token'))) {
